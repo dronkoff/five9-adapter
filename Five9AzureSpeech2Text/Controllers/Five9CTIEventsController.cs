abc3e40d6d7087d8fe619317867aafa2256c53b5
Vladimir Dronov <vdronov@microsoft.com>
2024-11-26 15:28:57 -0500

    added azd files and api controller

---
+++ Five9AzureSpeech2Text/Controllers/Five9CTIEventsController.cs
@@ -0,0 +1,42 @@
+﻿using Five9AzureSpeech2Text.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Five9AzureSpeech2Text.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Five9CTIEventsController : ControllerBase
+    {
+        private readonly ILogger<Five9CTIEventsController> _logger;
+        private readonly IConfiguration _config;
+
+        public Five9CTIEventsController(IConfiguration config, ILogger<Five9CTIEventsController> logger)
+        {
+            _config = config;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Get called. Responding with token.");
+            var trustToken = _config["Five9TrustToken"] ?? throw new Exception("Five9TrustToken is not configured.");
+            return Content(trustToken, "application/json");
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] dynamic jsonData)
+        {
+            string s = jsonData.description;
+            _logger.LogInformation("Post called. Probably a CTI event. {0}", s);
+            return Ok();
+        }
+
+        //[HttpPost("GetSpeechToText")]
+        //public IActionResult GetSpeechToText([FromBody] IFormFile file)
+        //{
+        //    return Ok("GetSpeechToText");
+        //}
+    }
+}
--- Five9AzureSpeech2Text/Program.cs
+++ Five9AzureSpeech2Text/Program.cs
@@ -17,6 +17,8 @@ builder.AddServiceDefaults();
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddControllers();
+
 
 var app = builder.Build();
 
@@ -25,6 +27,8 @@ app.MapDefaultEndpoints();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<Five9VoiceService>();
+app.MapControllers();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
 
 app.Run();
